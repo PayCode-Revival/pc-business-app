645f20305a996d3f90c52f31c3f0fe378104237e
Idowu Damilare Muyiwa Gabriel <dammy@damilareidowu.com>
2022-07-04 17:17:01 +0100

    Axios Base URL Updated

--- src/statics/api.js
+++ src/statics/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const bearerToken = localStorage.getItem("bearer-token")
 export const api = axios.create({
-    baseURL: `http://localhost:8000/api/`,
+    baseURL: `http://192.168.68.104:8000/api/`,
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${bearerToken}`,
